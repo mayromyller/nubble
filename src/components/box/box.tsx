d339ab1eab099fce39ec9e9249ba1a4cf9499164
Mayro <deehmyller11@gmail.com>
2023-10-17 15:38:38 -0300

    chore: create a custom box component

---
+++ src/components/box/box.tsx
@@ -0,0 +1,36 @@
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
+
+import {
+  BackgroundColorProps,
+  BorderProps,
+  LayoutProps,
+  SpacingProps,
+  SpacingShorthandProps,
+  backgroundColor,
+  border,
+  createBox,
+  createRestyleComponent,
+  layout,
+  spacing,
+  spacingShorthand
+} from '@shopify/restyle'
+
+import { Theme } from '../../theme/theme'
+
+export const Box = createBox<Theme>()
+export type BoxProps = React.ComponentProps<typeof Box>
+
+export type TouchableOpacityBoxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  TouchableOpacityProps
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, spacingShorthand, layout, border],
+  TouchableOpacity
+)
