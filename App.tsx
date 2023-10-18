e8a6e00fdd5fbfbbee34e4da6f20ab847cfb1102
Mayro <deehmyller11@gmail.com>
2023-10-18 16:01:12 -0300

    chore: add icons components and svgs

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
 import { SafeAreaView } from 'react-native'
+import { ThemeProvider } from '@shopify/restyle'
+
 import { Text } from './src/components/text/text'
+
+import { theme } from './src/theme/theme'
+import { Icon } from './src/components/icons/icon'
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text preset="headingLarge" italic>
-        NubbleApp Testing
-      </Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge" italic>
+          NubbleApp Testing
+        </Text>
+
+        <Icon name="eyeOn" color="carrotSecondaryLight" />
+        <Icon name="eyeOff" color="greenPrimary" />
+      </SafeAreaView>
+    </ThemeProvider>
   )
 }
