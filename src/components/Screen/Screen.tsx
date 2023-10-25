8e4b5772302ad5782ef7c497528fe34f76db842e
Mayro <deehmyller11@gmail.com>
2023-10-25 11:59:32 -0300

    update: add custom hook safeArea in Screen component

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
 import { Box } from '../box/box'
+import { useSafeArea } from '../../hooks/useSafeArea'
 
 interface ScreenProps {
   children: React.ReactNode
 }
 
 export function Screen({ children }: ScreenProps) {
+  const { top } = useSafeArea()
+
   return (
-    <Box paddingHorizontal="s24" bg="grayWhite" flex={1}>
+    <Box
+      paddingHorizontal="s24"
+      bg="grayWhite"
+      flex={1}
+      style={{
+        paddingTop: top
+      }}>
       {children}
     </Box>
   )
