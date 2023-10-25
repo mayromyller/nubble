d01a6255163550a9c73b5acecb015b968a1c43f8
Mayro <deehmyller11@gmail.com>
2023-10-25 16:13:14 -0300

    update: add Pressable option in icon component

--- src/components/icons/icon.tsx
+++ src/components/icons/icon.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+
+import { Pressable } from 'react-native'
+
 import { EyeOffIcon } from '../../assets/icons/EyeOffIcon'
 import { EyeOnIcon } from '../../assets/icons/EyeOnIcon'
+import { ArrowLeftIcon } from '../../assets/icons/ArrowLeftIcon'
 import { ThemeColors } from '../../theme/theme'
 import { useAppTheme } from '../../hooks/useAppTheme'
 
@@ -12,19 +16,35 @@ export type IconBase = {
 interface Props {
   name: IconName
   color?: ThemeColors
+  onPress?: () => void
 }
 
-export function Icon({ name, color = 'backgroundContrast' }: Props) {
+export function Icon({ name, color = 'backgroundContrast', onPress }: Props) {
   const { colors } = useAppTheme()
 
   const SVGIcon = iconRegistry[name]
+
+  if (onPress) {
+    return (
+      <Pressable
+        style={{
+          flex: 1,
+          justifyContent: 'center'
+        }}
+        hitSlop={10}
+        onPress={onPress}>
+        <SVGIcon color={colors[color]} />
+      </Pressable>
+    )
+  }
 
   return <SVGIcon color={colors[color]} />
 }
 
 const iconRegistry = {
   eyeOff: EyeOffIcon,
-  eyeOn: EyeOnIcon
+  eyeOn: EyeOnIcon,
+  arrowLeft: ArrowLeftIcon
 }
 
 type IconType = typeof iconRegistry
