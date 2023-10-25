c5833031ee008eee99f1bff95d11a911473df422
Mayro <deehmyller11@gmail.com>
2023-10-25 11:04:33 -0300

    chore: add text-input component

---
+++ src/components/text-input/text-input.tsx
@@ -0,0 +1,73 @@
+import React, { useRef } from 'react'
+
+import {
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+  TextStyle,
+  Pressable
+} from 'react-native'
+
+import { Box, BoxProps } from '../box/box'
+import { $fontFamily, $fontSizes, Text } from '../text/text'
+import { useAppTheme } from '../../hooks/useAppTheme'
+
+interface TextInputProps extends RNTextInputProps {
+  label: string
+  errorMessage?: string
+  RightComponent?: React.ReactElement
+}
+
+export function TextInput({
+  label,
+  errorMessage,
+  RightComponent,
+  ...rnTextInputProps
+}: TextInputProps) {
+  const { colors } = useAppTheme()
+  const inputRef = useRef<RNTextInput>(null)
+  const focusInput = () => inputRef.current?.focus()
+
+  const $textInputContainer: BoxProps = {
+    flexDirection: 'row',
+    borderWidth: errorMessage ? 2 : 1,
+    padding: 's16',
+    borderColor: errorMessage ? 'error' : 'gray4',
+    borderRadius: 's12'
+  }
+
+  return (
+    <Pressable onPress={focusInput}>
+      <Box>
+        <Text preset="paragraphMedium" marginBottom="s4">
+          {label}
+        </Text>
+        <Box {...$textInputContainer}>
+          <RNTextInput
+            ref={inputRef}
+            placeholderTextColor={colors.gray2}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+          {RightComponent && (
+            <Box justifyContent="center" ml="s16">
+              {RightComponent}
+            </Box>
+          )}
+        </Box>
+        {errorMessage && (
+          <Text preset="paragraphSmall" bold color="error">
+            {errorMessage}
+          </Text>
+        )}
+      </Box>
+    </Pressable>
+  )
+}
+
+const $textInputStyle: TextStyle = {
+  flexGrow: 1,
+  flexShrink: 1,
+  padding: 0,
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium
+}
