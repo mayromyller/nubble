6feaa217f59f21da0c70b9fed2c468c1fc43d595
Mayro <deehmyller11@gmail.com>
2023-10-25 16:15:53 -0300

    chore: add signUp screen on App to view component render

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { theme } from './src/theme/theme'
 import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen'
+import { SignUp } from './src/screens/auth/SignUp/SignUp'
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <LoginScreen />
+        {/* <LoginScreen /> */}
+        <SignUp />
       </ThemeProvider>
     </SafeAreaProvider>
   )
