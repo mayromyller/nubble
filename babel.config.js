6dcc169cd45ae19921aaa2d9797422c7f552bf9e
Mayro <deehmyller11@gmail.com>
2023-11-07 15:37:57 -0300

    style: add some absolute paths

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,11 @@ module.exports = {
       {
         root: '.',
         alias: {
-          '@components': './src/components'
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@theme': './src/theme'
         }
       }
     ]
