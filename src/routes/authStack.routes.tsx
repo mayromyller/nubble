31e120f42dd7abf04f30edd3ce04af6fd51fd663
Mayro <deehmyller11@gmail.com>
2023-11-14 10:55:42 -0300

    chore: change router and create app and auth stack routes

---
+++ src/routes/authStack.routes.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { IconProps } from '../components/icons/icon'
+import { ForgotPasswordScreen } from '../screens/auth/ForgotPasswordScreen/ForgotPasswordScreen'
+import { LoginScreen } from '../screens/auth/LoginScreen/LoginScreen'
+import { SignUpScreen } from '../screens/auth/SignUp/SignUpScreen'
+import { SuccessScreen } from '../screens/auth/Success/SuccessScreen'
+
+export type AuthStackParamList = {
+  LoginScreen: undefined
+  SignUpScreen: undefined
+  SuccessScreen: {
+    title: string
+    description: string
+    icon: Pick<IconProps, 'name' | 'color'>
+  }
+  ForgotPasswordScreen: undefined
+}
+
+const Stack = createNativeStackNavigator<AuthStackParamList>()
+
+export function AuthStackRouter() {
+  return (
+    <Stack.Navigator
+      initialRouteName="LoginScreen"
+      screenOptions={{
+        headerShown: false,
+        fullScreenGestureEnabled: true
+      }}>
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+      <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
+      <Stack.Screen
+        name="ForgotPasswordScreen"
+        component={ForgotPasswordScreen}
+      />
+    </Stack.Navigator>
+  )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,44 +1,15 @@
 import React from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import { IconProps } from '../components/icons/icon'
-import { ForgotPasswordScreen } from '../screens/auth/ForgotPasswordScreen/ForgotPasswordScreen'
-import { LoginScreen } from '../screens/auth/LoginScreen/LoginScreen'
-import { SignUpScreen } from '../screens/auth/SignUp/SignUpScreen'
-import { SuccessScreen } from '../screens/auth/Success/SuccessScreen'
-
-export type RootStackParamList = {
-  LoginScreen: undefined
-  SignUpScreen: undefined
-  SuccessScreen: {
-    title: string
-    description: string
-    icon: Pick<IconProps, 'name' | 'color'>
-  }
-  ForgotPasswordScreen: undefined
-}
-
-const Stack = createNativeStackNavigator<RootStackParamList>()
+import { AppStackRouter } from './appStack.routes'
+import { AuthStackRouter } from './authStack.routes'
 
 export function Router() {
+  const isLogged = true
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="LoginScreen"
-        screenOptions={{
-          headerShown: false,
-          fullScreenGestureEnabled: true
-        }}>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-        <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
-        <Stack.Screen
-          name="ForgotPasswordScreen"
-          component={ForgotPasswordScreen}
-        />
-      </Stack.Navigator>
+      {isLogged ? <AppStackRouter /> : <AuthStackRouter />}
     </NavigationContainer>
   )
 }
