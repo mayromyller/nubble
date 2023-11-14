0aef181f65fea3c8d2bd504d9e5c9302aef96bfb
Mayro <deehmyller11@gmail.com>
2023-11-14 10:58:22 -0300

    chore: add Home and Settings screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,6 @@ export * from './auth/ForgotPasswordScreen/ForgotPasswordScreen'
 export * from './auth/LoginScreen/LoginScreen'
 export * from './auth/SignUp/SignUpScreen'
 export * from './auth/Success/SuccessScreen'
+
+export * from './app/HomeScreen/HomeScreen'
+export * from './app/SettingsScreen/SettingsScreen'
