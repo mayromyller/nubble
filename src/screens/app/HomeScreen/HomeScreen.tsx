187457b4df1e1d968df1644c898b1dd03585b317
Mayro <deehmyller11@gmail.com>
2023-11-29 15:56:10 -0300

    chore: composite home screen with PostItem component in FlatList

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,22 +1,47 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import {
+  FlatList,
+  ListRenderItemInfo,
+  StyleProp,
+  ViewStyle
+} from 'react-native'
 
-import { Button, Screen, Text } from '@components'
+import { postService, Post } from '@domain'
+
+import { Button, Screen, Text, PostItem } from '@components'
 import { AppTabScreenProps } from '@routes'
 
-export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
-  return (
-    <Screen>
-      <Text preset="headingLarge">Home Screen</Text>
-      <Button
-        title="Settings"
-        onPress={() => navigation.navigate('SettingsScreen')}
-      />
+import { HomeHeader } from './components/HomeHeader'
 
-      <Button
-        title="Favorite"
-        mt="s10"
-        onPress={() => navigation.navigate('FavoriteScreen')}
+export function HomeScreen({ navigation }: AppTabScreenProps<'HomeScreen'>) {
+  const [postList, setPostLit] = useState<Post[]>([])
+  const { getList } = postService
+
+  useEffect(() => {
+    getList().then(list => setPostLit(list))
+  }, [])
+
+  function renderItem({ item }: ListRenderItemInfo<Post>) {
+    return <PostItem post={item} />
+  }
+
+  return (
+    <Screen style={$screen}>
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={<HomeHeader />}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
       />
     </Screen>
   )
+}
+
+const $screen: StyleProp<ViewStyle> = {
+  paddingHorizontal: 0,
+  paddingBottom: 0,
+  paddingTop: 0
 }
