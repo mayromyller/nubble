28af2fe90569a9addda67d233227212cdeb092a8
Mayro <deehmyller11@gmail.com>
2023-12-05 16:22:32 -0300

    chore: update fetch api with axios instance

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,18 +1,10 @@
-import { PageAPI } from '@api'
+import { PageAPI, api } from '@api'
 
 import { PostAPI } from './postTypes'
 
 async function getList(): Promise<PageAPI<PostAPI>> {
-  let response = await fetch('http://192.168.0.109:3333/user/post', {
-    method: 'GET',
-    headers: {
-      Authorization:
-        'Bearer Mg.G_5cPdhANNzPViLVNk0zwrD0qHtlEgf-JGlESBbGtdF2h6RZn_D-JsTkCwUT'
-    }
-  })
-
-  let data = await response.json()
-  return data
+  const response = await api.get<PageAPI<PostAPI>>('user/post')
+  return response.data
 }
 
 export const postApi = {
