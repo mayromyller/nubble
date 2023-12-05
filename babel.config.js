1b0dc140edc0c02e9e4c6652bab6b600968161af
Mayro <deehmyller11@gmail.com>
2023-12-05 16:07:22 -0300

    chore: add types to Page API and config paths

--- babel.config.js
+++ babel.config.js
@@ -12,7 +12,8 @@ module.exports = {
           '@screens': './src/screens',
           '@theme': './src/theme',
           '@domain': './src/domain',
-          '@brand': './src/brand'
+          '@brand': './src/brand',
+          '@api': './src/api'
         }
       }
     ]
